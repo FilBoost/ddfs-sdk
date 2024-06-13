b8158da8d2bf6c9d89f2c075eced3545a58da7e3
nemo <nemo1002forwork@gmail.com>
2024-06-13 12:36:59 +0800

    add TTDDfsHTTP

--- market/types.go
+++ market/types.go
@@ -3,11 +3,12 @@ package market
 const (
 	TTDDfsSync = "DDFSSync"
 	TTDDfsUrl  = "DDFSUrl"
+	TTDDfsHTTP = "DDFShttp"
 )
 
 func IsDDTransferType(transferType string) bool {
 	switch transferType {
-	case TTDDfsSync, TTDDfsUrl:
+	case TTDDfsSync, TTDDfsUrl, TTDDfsHTTP:
 		return true
 	}
 	return false
